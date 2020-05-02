c6ad95d8a9ff1b9604ab85ceb65f2350db53c69e
fachsimpeln <35078533+fachsimpeln@users.noreply.github.com>
2020-05-02 14:15:56 +0200

    Added config & index

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+     // ReCaptcha settings
+     $recaptcha_secret = "<YOUR_RECAPTCHA_SECRET>";
+     $recaptcha_public = "<YOUR_RECAPTCHA_SITE_KEY>";
+
+     // Dashboard creds
+     $admin_user = "admin";
+     $admin_password = "<PLEASE_CHANGE>";
+
+     // Path for data (optional)
+     $base_path = __DIR__ . "/../_data/";
+
+?>
